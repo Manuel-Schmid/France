72fdad866e707b762a11f933cc552273ea514ddc
Manuel Schmid <manysch3@gmail.com>
2021-10-06 16:56:15 +0200

    added header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root', // with this tag the component will be integrated into the html ( <app-root><app-root/> )
+  templateUrl: './app.component.html', // html-file
+  styleUrls: ['./app.component.scss'] // stylesheet(s)
 })
-export class AppComponent {
-  title = 'France';
+export class AppComponent { // here you can add properties and methods (lifecycle-methods)
+  title: string = 'France'; // could be directly used in HTML with: {{ title }}
 }
