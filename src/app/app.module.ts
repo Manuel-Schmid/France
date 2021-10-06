1b735a91362912285a0ef8ae07ca434711b09b69
Manuel Schmid <manysch3@gmail.com>
2021-10-06 23:23:26 +0200

    implemented route-handling, added footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { GalleryItemComponent } from './components/gallery-item/gallery-item.com
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule, Routes } from "@angular/router";
 import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HeaderComponent }
+  { path: '', component: HeaderComponent },
+  { path: 'about', component: AboutComponent },
 ]
 
 @NgModule({
@@ -19,6 +21,7 @@ const appRoutes: Routes = [
     HeaderComponent,
     GalleryItemComponent,
     AboutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
